853eae2c2a9d65813673bec3b9cc249d74d9171c
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-20 14:19:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
+
+<template>
+  <header></header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<style>
+
+</style>
